0b665bc34402230944f112dd2ed3f3ce3a4dc2af
DjQris <gen.chriscruz@outlook.com>
2022-10-14 15:37:53 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - returns alphabelts
+ * Return: returns 0 (success)
+ */
+int main(void)
+{
+	int ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
