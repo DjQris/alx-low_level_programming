4b80e299e20a96fb4cfc960a6c8b4cf0896aa492
DjQris <gen.chriscruz@outlook.com>
2022-10-18 12:32:02 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+int i, ten;
+
+ten = 0;
+
+while (ten < 10)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+																		}
+ten++;
+_putchar('\n');
+}
+}
