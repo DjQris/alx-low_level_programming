a0bef9a1f6200fd023611dbceb27f98987942e34
Qristal_Dev <113688701+DjQris@users.noreply.github.com>
2022-11-26 00:48:11 +0100

    Edit Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,45 +1,40 @@
-#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
+
 #include "lists.h"
 
 /**
- * add_node - adds a node to the beginning of a linked list
- * @head: pointer to the head of the list
- * @str: string to be used as content of the node
+ * add_node - Print strings
+ * @head: pointer to list node
+ * @str: string (value) of list node
  *
- * Return: address of the newly added node
+ * Return: pointer to newly created node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
 
+	if (str == NULL)
+		return (NULL);
+
 	new_node = malloc(sizeof(list_t));
-	if (new_node != NULL)
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+
+	if (head == NULL)
 	{
-		new_node->str = strdup(str);
-		new_node->len = _strlen(str);
-		new_node->next = *head;
+		new_node->next = NULL;
+		head = &new_node;
+		return (*head);
 	}
 	else
-		return (NULL);
-	if (*head != NULL)
+	{
 		new_node->next = *head;
-	*head = new_node;
+		*head = new_node;
+	}
+
 	return (new_node);
-}
-
-/**
- * _strlen - counts the length of a string
- * @str: string to be counted
- *
- * Return: the length of the string
- */
-int _strlen(const char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-		i++;
-	return (i);
 }
