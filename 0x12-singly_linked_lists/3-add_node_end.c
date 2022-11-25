f0e9f1c237ce09dc0a67f7b7d17cf37dc332046d
Qristal_Dev <113688701+DjQris@users.noreply.github.com>
2022-11-26 00:50:19 +0100

    Edit Task 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,52 +1,47 @@
-#include "lists.h"
-#include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdlib.h>
+
+#include "lists.h"
 
 /**
- *  * add_node_end - adds a node to the end of a linked list
- * @head: pointer to the head of the list
- * @str: string to be used as content of the newly added node
+ * add_node_end - add node to end of list
+ * @head: pointer to list node
+ * @str: string (value) of list node
  *
- * Return: address of the newly added node
+ * Return: pointer to newly created node
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
-	list_t *cursor = *head;
+
+	if (str == NULL)
+		return (NULL);
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node != NULL)
-	{
-		new_node->str = strdup(str);
-		new_node->len = _strlen(str);
-		new_node->next = NULL;
-	}
-	else
+	if (new_node == NULL)
 		return (NULL);
-	if (cursor != NULL)
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (head == NULL)
 	{
-		while (cursor->next != NULL)
-			cursor = cursor->next;
-		cursor->next = new_node;
+		head = &new_node;
+		return (*head);
+	}
+	else if (*head == NULL)
+	{
+		*head = new_node;
 	}
 	else
-		*head = new_node;
+	{
+		list_t *tmp = *head;
+
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
+	}
+
 	return (new_node);
-}
-
-
-/**
- * _strlen - counts the length of a string
- * @str: string to be counted
- *
- * Return: the length of the string
- */
-int _strlen(const char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-		i++;
-	return (i);
 }
